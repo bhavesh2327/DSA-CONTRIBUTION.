0b77842305980274e005dc0dcfcc7542ccb6664b
V Bhavesh <143629789+bhavesh2327@users.noreply.github.com>
2026-01-11 16:53:57 +0530

    Update leetcode389. Find _the _differences.cpp

--- leetcode389. Find _the _differences.cpp
+++ leetcode389. Find _the _differences.cpp
@@ -1,3 +1,7 @@
+
+using the unordered map 
+
+
 class Solution {
 public:
     char findTheDifference(string s, string t) {
@@ -13,5 +17,23 @@ public:
         }
         }
         return 'x';
+    }
+};
+
+
+using the xor method!!
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int XOR = 0;
+        for(char &ch : s) {
+            XOR ^= ch;
+        }
+        for(char &ch : t) {
+            XOR ^= ch;
+        }
+        return (char)XOR;
+
     }
 };
